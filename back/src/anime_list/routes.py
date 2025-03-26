96145d1bbe2b840df198954c7eb93512e9e18077
fmadera <fmadera@cerberussolutions.io>
2025-03-26 17:25:25 -0400

    refactor anime list

--- back/src/anime_list/routes.py
+++ back/src/anime_list/routes.py
@@ -1,93 +1,39 @@
-from aiomysql import Error as ConnectionError, DictCursor
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 
 from .models import AnimeListCreate, AnimeListUpdate
-from shared.database import Database
+from .service import AnimeListService
 from shared.security import get_current_user
+from users.models import User
 
 router = APIRouter()
 
 
 @router.get("/")
 async def get_anime_lists():
-    try:
-        async with await Database.get_connection() as con:
-            async with con.cursor(DictCursor) as cursor:
-                await cursor.execute("CALL sp_anime_lists(NULL, NULL, NULL, %s)", (1,))
-                results = await cursor.fetchall()
-
-            if not results:
-                raise HTTPException(status_code=400, detail="Anime lists not found")
-
-            return results
-    except ConnectionError as e:
-        raise HTTPException(status_code=500, detail=f"Error: {e}")
+    return await AnimeListService.get_all_anime_lists()
 
 
 @router.get("/{list_id}")
 async def get_anime_list(list_id: str):
-    try:
-        async with await Database.get_connection() as con:
-            async with con.cursor(DictCursor) as cursor:
-                await cursor.execute(
-                    "CALL sp_anime_lists(%s, NULL, NULL, %s)", (list_id, 2)
-                )
-                result = await cursor.fetchone()
-
-            if not result:
-                raise HTTPException(status_code=400, detail="Anime list not found")
-
-            return result
-    except ConnectionError as e:
-        raise HTTPException(status_code=500, detail=f"Error: {e}")
+    return await AnimeListService.get_anime_list_by_id(list_id)
 
 
 @router.post("/")
 async def post_anime_list(
-    anime_list: AnimeListCreate, current_user: str = Depends(get_current_user)
+    anime_list: AnimeListCreate, current_user: User = Depends(get_current_user)
 ):
-    try:
-        async with await Database.get_connection() as con:
-            async with con.cursor(DictCursor) as cursor:
-                await cursor.execute(
-                    "CALL sp_anime_lists(NULL, %s, %s, %s)",
-                    (anime_list.list_name, current_user.user_id, 3),
-                )
-            await con.commit()
-            return {"message": "Anime list created successfully"}
-    except ConnectionError as e:
-        raise HTTPException(status_code=500, detail=f"Error: {e}")
+    return await AnimeListService.create_anime_list(anime_list, current_user)
 
 
 @router.put("/")
 async def put_anime_list(
-    anime_list: AnimeListUpdate, current_user: str = Depends(get_current_user)
+    anime_list: AnimeListUpdate, current_user: User = Depends(get_current_user)
 ):
-    try:
-        async with await Database.get_connection() as con:
-            async with con.cursor(DictCursor) as cursor:
-                await cursor.execute(
-                    "CALL sp_anime_lists(%s, %s, %s, %s)",
-                    (anime_list.list_id, anime_list.name, current_user.user_id, 4),
-                )
-            await con.commit()
-            return {"message": "Anime list updated successfully"}
-    except ConnectionError as e:
-        raise HTTPException(status_code=500, detail=f"Error: {e}")
+    return await AnimeListService.update_anime_list(anime_list, current_user)
 
 
 @router.delete("/{list_id}")
 async def delete_anime_list(
-    list_id: str, current_user: str = Depends(get_current_user)
+    list_id: str, current_user: User = Depends(get_current_user)
 ):
-    try:
-        async with await Database.get_connection() as con:
-            async with con.cursor(DictCursor) as cursor:
-                await cursor.execute(
-                    "CALL sp_anime_lists(%s, NULL, %s, %s)",
-                    (list_id, current_user.user_id, 5),
-                )
-            await con.commit()
-            return {"message": "Anime list deleted successfully"}
-    except ConnectionError as e:
-        raise HTTPException(status_code=500, detail=f"Error: {e}")
+    return await AnimeListService.delete_anime_list(list_id, current_user)
---
+++ back/src/anime_list/service.py
@@ -0,0 +1,84 @@
+from aiomysql import Error as ConnectionError, DictCursor
+from fastapi import HTTPException
+
+from .models import AnimeListCreate, AnimeListUpdate
+from shared.database import Database
+from users.models import User
+
+
+class AnimeListService:
+    @staticmethod
+    async def get_all_anime_lists():
+        try:
+            async with await Database.get_connection() as con:
+                async with con.cursor(DictCursor) as cursor:
+                    await cursor.execute(
+                        "CALL sp_anime_lists(NULL, NULL, NULL, %s)", (1,)
+                    )
+                    results = await cursor.fetchall()
+
+                if not results:
+                    raise HTTPException(status_code=400, detail="Anime lists not found")
+
+                return results
+        except ConnectionError as e:
+            raise HTTPException(status_code=500, detail=f"Error: {e}")
+
+    @staticmethod
+    async def get_anime_list_by_id(list_id: str):
+        try:
+            async with await Database.get_connection() as con:
+                async with con.cursor(DictCursor) as cursor:
+                    await cursor.execute(
+                        "CALL sp_anime_lists(%s, NULL, NULL, %s)", (list_id, 2)
+                    )
+                    result = await cursor.fetchone()
+
+                if not result:
+                    raise HTTPException(status_code=400, detail="Anime list not found")
+
+                return result
+        except ConnectionError as e:
+            raise HTTPException(status_code=500, detail=f"Error: {e}")
+
+    @staticmethod
+    async def create_anime_list(anime_list: AnimeListCreate, user: User):
+        try:
+            async with await Database.get_connection() as con:
+                async with con.cursor(DictCursor) as cursor:
+                    await cursor.execute(
+                        "CALL sp_anime_lists(NULL, %s, %s, %s)",
+                        (anime_list.list_name, user.user_id, 3),
+                    )
+                await con.commit()
+                return {"message": "Anime list created successfully"}
+        except ConnectionError as e:
+            raise HTTPException(status_code=500, detail=f"Error: {e}")
+
+    @staticmethod
+    async def update_anime_list(anime_list: AnimeListUpdate, user: User):
+        try:
+            async with await Database.get_connection() as con:
+                async with con.cursor(DictCursor) as cursor:
+                    await cursor.execute(
+                        "CALL sp_anime_lists(%s, %s, %s, %s)",
+                        (anime_list.list_id, anime_list.name, user.user_id, 4),
+                    )
+                await con.commit()
+                return {"message": "Anime list updated successfully"}
+        except ConnectionError as e:
+            raise HTTPException(status_code=500, detail=f"Error: {e}")
+
+    @staticmethod
+    async def delete_anime_list(list_id: str, user: User):
+        try:
+            async with await Database.get_connection() as con:
+                async with con.cursor(DictCursor) as cursor:
+                    await cursor.execute(
+                        "CALL sp_anime_lists(%s, NULL, %s, %s)",
+                        (list_id, user.user_id, 5),
+                    )
+                await con.commit()
+                return {"message": "Anime list deleted successfully"}
+        except ConnectionError as e:
+            raise HTTPException(status_code=500, detail=f"Error: {e}")
